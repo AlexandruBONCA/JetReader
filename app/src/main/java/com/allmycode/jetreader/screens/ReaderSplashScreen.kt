91c501cdc18201bc69fbe9e2e98d549a4e95271b
Alexandru Bonca <bonca_alex@yahoo.com>
2026-09-11 15:02:46 +0300

    Implemented Animation for ReaderSplashScreen

--- app/src/main/java/com/allmycode/jetreader/screens/ReaderSplashScreen.kt
+++ app/src/main/java/com/allmycode/jetreader/screens/ReaderSplashScreen.kt
@@ -1,10 +1,66 @@
 package com.allmycode.jetreader.screens
 
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavHostController
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import kotlinx.coroutines.delay
 
+@Preview
 @Composable
-fun SplashScreen(navController: NavHostController) {
-    Text("Welcome to splash Screen")
+fun SplashScreen(navController: NavController = NavController(context = LocalContext.current)) {
+    val scale = remember {
+        Animatable(0f)
+    }
+    LaunchedEffect(Unit) {
+        scale.animateTo(targetValue = 0.9f,
+            animationSpec = tween(durationMillis = 800,
+                easing = {
+                    OvershootInterpolator(10f)
+                        .getInterpolation(it)
+                }))
+        delay(2000L)
+    }
+
+    Surface(modifier = Modifier
+        .padding(15.dp)
+        .size(330.dp)
+        .scale(scale.value),
+        color = Color.White,
+        border = BorderStroke(2.dp, Color.LightGray),
+        shape = CircleShape
+    ) {
+        Column(modifier = Modifier.padding(5.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center) {
+            Text("A. Reader", style = MaterialTheme.typography.displayLarge,
+                color = Color.Red.copy(alpha = 0.5f))
+            Spacer(modifier = Modifier.height(15.dp))
+            Text("\"Read. Pause. Reflect\"",
+                style = MaterialTheme.typography.headlineMedium,
+                color = Color.LightGray
+            )
+        }
+    }
 }
