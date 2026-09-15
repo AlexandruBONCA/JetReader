47db52371e942f7a4a458185887b5750fae6d3fd
Alexandru Bonca <bonca_alex@yahoo.com>
2026-09-15 14:54:01 +0300

    Adding Login and Create Account Logic

--- app/src/main/java/com/allmycode/jetreader/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/allmycode/jetreader/screens/login/ReaderLoginScreen.kt
@@ -2,12 +2,16 @@ package com.allmycode.jetreader.screens.login
 
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
@@ -32,7 +36,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -41,6 +47,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.allmycode.jetreader.R
 import com.allmycode.jetreader.components.EmailInput
 import com.allmycode.jetreader.components.PasswordInput
 import com.allmycode.jetreader.components.ReaderLogo
@@ -48,16 +55,37 @@ import kotlin.math.sin
 
 @Composable
 fun LoginScreen(navController: NavController) {
-
+    val showLoginForm = rememberSaveable { mutableStateOf(true)}
     Surface(modifier = Modifier
         .fillMaxSize()) {
 
         Column(horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Top) {
             ReaderLogo()
-            UserForm(loading = false, isCreateAccount = false) {email, password ->
-                Log.d("Form", "ReaderLoginScreen: $email, $password")
+            if (showLoginForm.value) {
+                UserForm(loading = false, isCreateAccount = false) {email, password ->
+                    Log.d("Form", "ReaderLoginScreen: $email, $password")
+                    //TO DO: FB login
+                }
+            } else {
+                UserForm(loading = false, isCreateAccount = true) { email, password ->
+                    //To DO: create FB account
+                }
             }
+        }
+        Spacer(modifier = Modifier.size(15.dp))
+        Row(
+            modifier = Modifier.padding(15.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center) {
+            val text = if (showLoginForm.value) "Sign up" else "Login"
+            Text(text = if (showLoginForm.value) "New user?" else "Already a user?")
+            Text(text,
+                modifier = Modifier.clickable {
+                    showLoginForm.value = !showLoginForm.value
+                }.padding(start = 5.dp,),
+                fontWeight = FontWeight.Bold,
+                color = MaterialTheme.colorScheme.onSecondaryContainer)
         }
     }
 }
@@ -82,6 +110,8 @@ fun UserForm(
         .background(MaterialTheme.colorScheme.background)
         .verticalScroll(rememberScrollState())
 
+    if (isCreateAccount) Text(text = stringResource(id = R.string.create_act),
+        modifier = Modifier.padding(4.dp)) else Text("")
     Column(modifier,
         horizontalAlignment = Alignment.CenterHorizontally) {
         EmailInput(emailState = email,
@@ -106,6 +136,7 @@ fun UserForm(
         validInputs = valid
     ) {
         onDone(email.value.trim(), password.value.trim())
+        keyboardController?.hide()
     }
 }
 
