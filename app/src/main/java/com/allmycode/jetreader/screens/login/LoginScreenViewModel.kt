08e86c5c48609ec6ca5d20e60fc10323fdf1cdd4
Alexandru Bonca <bonca_alex@yahoo.com>
2026-09-15 17:40:42 +0300

    Create the MUser model

--- app/src/main/java/com/allmycode/jetreader/screens/login/LoginScreenViewModel.kt
+++ app/src/main/java/com/allmycode/jetreader/screens/login/LoginScreenViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.allmycode.jetreader.model.MUser
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
@@ -41,9 +42,12 @@ class LoginScreenViewModel: ViewModel() {
 
     private fun createUser(displayName: String?) {
         val userId = auth.currentUser?.uid
-        val user = mutableMapOf<String, Any>()
-        user["user_id"] = userId.toString()
-        user["displayName"] = displayName.toString()
+        val user = MUser(userId = userId.toString(),
+            displayName =  displayName.toString(),
+            avatarUrl = "",
+            quote = "Firebase is great",
+            profession = "Android Developer",
+            id = null).toMap()
 
         FirebaseFirestore.getInstance().collection("users").add(user)
     }
